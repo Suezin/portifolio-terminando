ff79f37ed8419f7bfae03627e0fee638e3803da9
Suezin <162615024+Suezin@users.noreply.github.com>
2024-10-02 22:37:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const navHome = document.getElementById("home");
+const navHabilidades = document.getElementById("Habilidades");
+const navProjetos = document.querySelector();
+const ativo = document.querySelector("a");
+navHome.addEventListener("click", function() {
+});
+navHabilidades.addEventListener("click", function() {
+    
+});
+navProjetos.addEventListener("click", function() {
+    navProjetos.Style.color= "green"
+    console.log("ola");
+});
